ceec9347a090a3fb610c6cb6dfbf68ac2cc4bbf9
George Sykes <georgesykes86@gmail.com>
2018-04-17 14:39:55 +0100

    Refactor tests for working? and release_bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,5 @@
 require 'bike'
 
 describe Bike do
-  it 'responds to the working? method' do
-    bike = Bike.new
-    expect(bike).to respond_to :working?
-  end
+  it { is_expected.to respond_to :working? }
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'responds to release bike' do
-    station = DockingStation.new
-    expect(station).to respond_to(:release_bike)
-  end
+  it { is_expected.to respond_to(:release_bike) }
 end
