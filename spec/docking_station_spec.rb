bbe3a753bd7136fa6ca248cb275a722a122042b7
George Sykes <georgesykes86@gmail.com>
2018-04-17 13:22:55 +0100

    Implemented respond to release_bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  it 'responds to release bike' do
+    station = DockingStation.new
+    expect(station).to respond_to(:release_bike)
+  end
 end
