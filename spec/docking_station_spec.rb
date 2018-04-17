412e975e892cdc20cf8a71b21db61493fca90475
Dione Estabillo <dione.estabillo@gmail.com>
2018-04-17 16:03:37 +0100

    Add tests to docking_station that gets bike and expects bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
+
   it { is_expected.to respond_to(:release_bike) }
+
+  it 'returns a bike' do
+    bike = subject.release_bike
+    expect(bike).to be_an_instance_of(Bike)
+  end
+
+  context 'when bike is working' do
+    it 'returns a working bike' do
+      bike = subject.release_bike
+      expect(bike.working?).to be true
+  end
+
+  end
 end
