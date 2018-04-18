a98cfdb528f3818227841ae0c7558beed382aa8c
Dione Estabillo <dione.estabillo@gmail.com>
2018-04-18 16:06:59 +0100

    corrected raising error functionality

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'returns a working bike' do
-      bike = subject.release_bike
+      #bike = subject.release_bike
+      bike = Bike.new
       expect(bike).to be_working
   end
 
@@ -16,19 +17,23 @@ describe DockingStation do
   it 'docks something' do
     bike  = Bike.new
     expect(subject.dock(bike)).to eq bike
-<<<<<<< HEAD
-=======
   end
 
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
->>>>>>> 590e7af9ee2fbabd240fae9143c202eb5d6fcf9b
   end
 
-  it 'raises error if no bike available' do
-      expect(subject.release_bike).to raise_error("no bikes available")
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect((subject.release_bike)).to eq bike
+    end
+    it 'raises error if no bikes available' do
+        expect { subject.release_bike }.to raise_error 'no bikes available'
+      end
   end
 
 end
