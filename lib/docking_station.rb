e6ed28b3bd1c7ee40779af6c4e27c01178270e9f
Asia Antczak <aasiaantczak@gmail.com>
2018-04-19 13:04:09 +0100

    add private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,18 +4,28 @@ class DockingStation
 
   attr_reader :bikes
 
-def initialize
-  @bikes = []
-end
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail 'no bikes available' if @bikes.length == 0
+    fail 'no bikes available' if empty?
     @bikes.pop
   end
 
+
   def dock(bike)
-    fail 'docking station is full' if @bikes.length == 20
+    fail 'docking station is full' if full?
     @bikes << bike
+  end
+
+  private
+  def empty?
+    @bikes.length == 0
+  end
+
+  def full?
+    @bikes.length == 20
   end
 
 end
